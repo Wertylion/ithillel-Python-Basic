2123f2e79f99681d6e52c54c947d5b84ea8b1b34
Dmytro <wertyliontv@gmail.com>
2026-03-03 21:11:43 +0100

    new text

---
+++ HM1.py
@@ -0,0 +1,5 @@
+import cowsay
+(cowsay.trex("Hello World"))
+
+print(cowsay.get_output_string('trex', 'hello'))
+
---
+++ HW2.py
@@ -0,0 +1,62 @@
+# задача 1
+
+# num1 = int(input("enter your number: "))
+# print(num1**2)
+
+# задача 2
+
+# number1 = int(input("enter your 1 number: "))
+# number2 = int(input("enter your 2 number: "))
+# number3 = int(input("enter your 3 number: "))
+# print((number1+number2+number3)/3)
+
+# задача 3
+
+# time = int(input("enter time for minutes: "))
+# hours = time // 60
+# minutes = time % 60
+# print("hours", hours, "minutes", minutes)
+
+# задача 4
+
+# prais = int(input("Enter prais:"))
+# percent = int(input("Enter percent:"))
+# print (prais - (percent * prais / 100))
+
+# задача 5
+
+# numbers = int(input("enter your number: "))
+# print(numbers % 10)
+
+# звдвча 6
+
+# length = int(input("enter your length: "))
+# width = int(input("enter your width: "))
+# s = (width + length) * 2
+# print("Perimeter:", s)
+
+# задача 7 v1
+
+# numeric = int(input("Enter a four-digit number: "))
+# number1 = numeric // 1000
+# number2 = numeric // 100 % 10
+# number3 = numeric // 10 % 10
+# number4 = numeric % 10
+#
+# print(number1)
+# print(number2)
+# print(number3)
+# print(number4)
+
+# задача 7 v2
+
+# numeric = int(input("Enter a four-digit number: "))
+# number1, remainder = divmod(numeric, 1000)
+# number2, remainder = divmod(remainder, 100)
+# number3, number4 = divmod(remainder, 10)
+# print(number1)
+# print(number2)
+# print(number3)
+# print(number4)
+
+
